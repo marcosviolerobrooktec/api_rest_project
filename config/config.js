7d1a8649621176243e68a63793f00f69ef9cf757
marcos.violero <marcos.violero@brooktec.com>
2025-02-17 13:44:01 +0100

    merge desarrollo

--- config/config.js
+++ config/config.js
@@ -1,10 +1,20 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const dotenv = require('dotenv');
+dotenv.config();
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-  });
-
-  module.exports = { sequelize };
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    define: {
+      freezeTableName: true,
+    },
+    auth: {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '12h',
+    },
+  },
+};
+module.exports = config;
---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const {sequelize} = require('../config/config');
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }, {
+    tableName: 'users',
+  });
+
+module.exports = User;
