b0f5712595f113ee508b7059dd77b854feb0913a
marcos.violero <marcos.violero@brooktec.com>
2025-02-18 10:57:29 +0100

    Seeders

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,10 @@ module.exports = (sequelize, DataTypes) => {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: true
     }
   });
 
