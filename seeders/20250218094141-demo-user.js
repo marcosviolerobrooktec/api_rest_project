965ea4d5362faec41a07527fc8cc9873562432bd
marcos.violero <marcos.violero@brooktec.com>
2025-02-19 12:14:19 +0100

    Correcion ruta foto y contraseña cifrada seeders

--- seeders/20250218094141-demo-user.js
+++ seeders/20250218094141-demo-user.js
@@ -1,25 +1,32 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const saltRounds = 10;
+    const hashedPasswords = await Promise.all([
+      bcrypt.hash('12345678', saltRounds),
+      bcrypt.hash('2342156143', saltRounds),
+      bcrypt.hash('2467243623', saltRounds)
+    ]);
     await queryInterface.bulkInsert('User', [
       {
         email: 'example@example.com',
-        password: '12345678',
+        password: hashedPasswords[0],
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         email: 'anotheruser@example.com',
-        password: '2342156143',
+        password: hashedPasswords[1],
         createdAt: new Date(),
         updatedAt: new Date(),
         age: 18,
       },
       {
         email: 'otheruser@example.com',
-        password: '2467243623',
+        password: hashedPasswords[2],
         createdAt: new Date(),
         updatedAt: new Date(),
         age: 22,
